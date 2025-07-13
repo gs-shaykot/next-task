0a97b0d755abcc5685fa63420c17986d0a1335a3
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-07-13 09:50:43 +0600

    endpoint link fix

--- src/app/api/reviews/route.js
+++ src/app/api/reviews/route.js
@@ -1,28 +1,6 @@
 import clientPromise from "../../../../lib/mongo";
-import Cors from 'cors';
-
-// Initialize the cors middleware
-const cors = Cors({
-  origin: 'https://next-task-plum.vercel.app', // Your frontend domain
-  methods: ['GET', 'POST', 'OPTIONS'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-});
-
-// Helper function to run middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
-
-export async function GET() {
-  // Run the cors middleware
-  await runMiddleware(new Request(), new Response(), cors);
+ 
+export async function GET() { 
 
   const client = await clientPromise;
   const db = client.db("CollegeDb");
